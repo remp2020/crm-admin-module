8d2ca6f26ffcd60160dc69722b577a4597faf2fd
Lubos Michalik <lubos.michalik@projektn.sk>
2022-01-25 11:50:49 +0100

    Add configurable options to require secure login for admin roles.
    Add handler for `UserSignInEvent` to resolve if user's authentication is secure.
    
    remp/crm#2091

---
+++ src/Seeders/ConfigsSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Crm\AdminModule\Seeders;
+
+use Crm\ApplicationModule\Builder\ConfigBuilder;
+use Crm\ApplicationModule\Config\ApplicationConfig;
+use Crm\ApplicationModule\Config\Repository\ConfigCategoriesRepository;
+use Crm\ApplicationModule\Config\Repository\ConfigsRepository;
+use Crm\ApplicationModule\Seeders\ConfigsTrait;
+use Crm\ApplicationModule\Seeders\ISeeder;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ConfigsSeeder implements ISeeder
+{
+    use ConfigsTrait;
+
+    private $configCategoriesRepository;
+
+    private $configsRepository;
+
+    private $configBuilder;
+
+    public function __construct(
+        ConfigCategoriesRepository $configCategoriesRepository,
+        ConfigsRepository $configsRepository,
+        ConfigBuilder $configBuilder
+    ) {
+        $this->configCategoriesRepository = $configCategoriesRepository;
+        $this->configsRepository = $configsRepository;
+        $this->configBuilder = $configBuilder;
+    }
+
+    public function seed(OutputInterface $output)
+    {
+        $category = $this->getCategory($output, 'users.config.category_authentication', 'fa fa-key', 300);
+
+        $this->addConfig(
+            $output,
+            $category,
+            'admin_secure_login_check',
+            ApplicationConfig::TYPE_BOOLEAN,
+            'admin.config.secure_login.name',
+            '',
+            false,
+            '290'
+        );
+    }
+}
