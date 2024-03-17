c927dc076be49d4c87f4e2d711412eb41a825fb0
Miroslav Svitok <mirosvitok@gmail.com>
2024-03-17 22:20:53 +0000

    Removed constraint of linking a single connected account of same type
    - Removed DB constraint
    - Adjusted UI
    - Security checks for admin accounts
    
    remp/crm#3105

--- src/Helpers/SecuredAdminAccess.php
+++ src/Helpers/SecuredAdminAccess.php
@@ -4,6 +4,7 @@ namespace Crm\AdminModule\Helpers;
 
 use Crm\ApplicationModule\Models\Config\ApplicationConfig;
 use Crm\UsersModule\Repositories\UserMetaRepository;
+use Crm\UsersModule\Repositories\UsersRepository;
 use Nette\Database\Table\ActiveRow;
 use Nette\Http\Session;
 
@@ -11,25 +12,33 @@ class SecuredAdminAccess
 {
     // Add this user meta flag only if user has a Google SSO account paired.
     public const USER_META_SECURE_LOGIN_ALLOWED = 'secure_login_allowed';
-
     public const ERROR_CODE_INSECURE_SESSION = 'insecure_session';
-
     public const ERROR_CODE_MISSING_USER_SECURE_FLAG = 'missing_user_secure_flag';
 
-    private Session $session;
-
-    private ApplicationConfig $applicationConfig;
-
-    private UserMetaRepository $userMetaRepository;
-
     public function __construct(
-        Session $session,
-        ApplicationConfig $applicationConfig,
-        UserMetaRepository $userMetaRepository
+        private Session $session,
+        private ApplicationConfig $applicationConfig,
+        private UserMetaRepository $userMetaRepository
     ) {
-        $this->session = $session;
-        $this->applicationConfig = $applicationConfig;
-        $this->userMetaRepository = $userMetaRepository;
+    }
+
+    public function canLinkOrUnlinkAccount(ActiveRow $user): bool
+    {
+        if ($user->role !== UsersRepository::ROLE_ADMIN) {
+            return true;
+        }
+
+        if (!$this->applicationConfig->get('admin_secure_login_check')) {
+            return true;
+        }
+
+        if (!$this->userMetaRepository->exists($user, self::USER_META_SECURE_LOGIN_ALLOWED)) {
+            return true;
+        }
+
+        // otherwise, do not enable linking/unlinking, since someone can bypass SSO authentication just by adding
+        // additional linked account and log in using that
+        return false;
     }
 
     public function setSecure(bool $value)
